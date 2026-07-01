c60702bacec28c61596cbac5770f12d2cf26faf2
infomarutiverse <info.marutiverse@gmail.com>
2026-07-01 21:30:10 +0530

    [ADD] app

--- mv_project_default_stages/__manifest__.py
+++ mv_project_default_stages/__manifest__.py
@@ -2,7 +2,7 @@
     "name": "Project Task Default Stages",
     "version": "19.0.1.0.0",
     "category": "Project",
-    "summary": "",
+    "summary": "Easily mark any existing or new project task stage as a Default Stage using a simple checkbox on the stage configuration form.",
     "author": "Marutiverse",
     "support": "support.marutiverse@gmail.com",
     "license": "LGPL-3",
